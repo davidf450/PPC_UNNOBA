13200c681e0b952cfc869d4cf7bc9462929fc1fa
David Fernandez <davidfernandez450@gmail.com>
2019-08-06 18:13:56 -0300

    agregado un dialog para reintentar comunicarse con el web service y un spinner de carga mientras lo hace.
    TODO: verificar si el spinner desaparece o queda sobre las obras

--- PPC_FINAL/src/main/java/ar/edu/unnoba/ppc/dfernandez/tp_final_ppc_unnoba/MainActivity.java
+++ PPC_FINAL/src/main/java/ar/edu/unnoba/ppc/dfernandez/tp_final_ppc_unnoba/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -16,6 +17,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.google.android.gms.location.FusedLocationProviderClient;
 
@@ -28,6 +30,7 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
 
@@ -45,11 +48,14 @@ public class MainActivity extends AppCompatActivity{
     RecyclerView listado_obras;
     ObrasAdapter adapter;
     FusedLocationProviderClient fusedLocationClient;
+    ProgressBar pg;
+    JsonObject json;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sharedPreferences = getSharedPreferences(LoginActivity.S_PREFERENCES, Context.MODE_PRIVATE);
+        pg = (ProgressBar) findViewById(R.id.loading_spinner);
         listado_obras = findViewById(R.id.recycler);
         //alert.showAlertDialog(MainActivity.this, "Bienvenido", "Bienvenido "+sharedPreferences.getString("user","invitado")+" al sistema de gestion de obras", false);
         cola = Volley.newRequestQueue(this);
@@ -64,6 +70,7 @@ public class MainActivity extends AppCompatActivity{
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -80,7 +87,8 @@ public class MainActivity extends AppCompatActivity{
                                 editor = sharedPreferences.edit();
                                 editor.clear();
                                 editor.apply();
-                                finish();
+                                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(i);
                             }
 
                         })
@@ -100,11 +108,39 @@ public class MainActivity extends AppCompatActivity{
                     @Override
                     public void onResponse(JSONArray response) {
                         llenar_lista(response);
+                        pg.setVisibility(View.GONE);
                     }
                 }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.stat_notify_error)
+                        .setTitle("Error")
+                        .setMessage("No es posible conectar con el web service en \n ["+url+"]")
+                        .setPositiveButton("Reintentar", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                                Intent i = getIntent();
+                                startActivity(i);
+                                }
+
+                        })
+                        .setNegativeButton("Salir", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                editor = sharedPreferences.edit();
+                                editor.clear();
+                                editor.apply();
+                                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(i);
+                            }
+
+                        })
+                        .show();
                 System.out.println("ERROR: hubo un error al conectar con el Web Service en [ "+url+" ]");
             }
         });
